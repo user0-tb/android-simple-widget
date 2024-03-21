afcff6f76978dff9ff05c47a593a01323544ad53
User0 <104380885+user0-tb@users.noreply.github.com>
2024-03-21 17:43:08 +0100

    Update WidgetService.java

--- app/src/main/java/com/tekseker/simplewidget/WidgetService.java
+++ app/src/main/java/com/tekseker/simplewidget/WidgetService.java
@@ -54,6 +54,72 @@ public final class WidgetService extends RemoteViewsService {
         }
 
         @Override
+            protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        final long DELAY_MILLIS = 750;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(R.layout.activity_main);
+                final WebView webView;
+                webView = (WebView)findViewById(R.id.webView);
+                WebSettings webSettings = webView.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+                webSettings.setDomStorageEnabled(true);
+                webView.setWebViewClient(new WebViewClient());
+                webView.setWebChromeClient(new WebChromeClient(){
+                    // For 3.0+ Devices (Start)
+                    // onActivityResult attached before constructor
+                    protected void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+                        mUploadMessage = uploadMsg;
+                        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                        i.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                        i.addCategory(Intent.CATEGORY_OPENABLE);
+                        i.setType("image/*");
+                        startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+                    }
+                    // For Lollipop 5.0+ Devices
+                    public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+                        if (uploadMessage != null) {
+                            uploadMessage.onReceiveValue(null);
+                            uploadMessage = null;
+                        }
+                        uploadMessage = filePathCallback;
+                        Intent intent = null;
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                            intent = fileChooserParams.createIntent();
+                        }
+                        try {
+                            startActivityForResult(intent, REQUEST_SELECT_FILE);
+                        } catch (ActivityNotFoundException e) {
+                            uploadMessage = null;
+                            Toast.makeText(getActivity().getApplicationContext(), "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
+                            return false;
+                        }
+                        return true;
+                    }
+                    //For Android 4.1 only
+                    protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                        mUploadMessage = uploadMsg;
+                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                        intent.addCategory(Intent.CATEGORY_OPENABLE);
+                        intent.setType("image/*");
+                        startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+                    }
+
+                    protected void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                        mUploadMessage = uploadMsg;
+                        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                        i.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                        i.addCategory(Intent.CATEGORY_OPENABLE);
+                        i.setType("image/*");
+                        startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+                    }
+                });
+
+        @Override
         public void onDestroy() {
             mWidgetItems.clear();
         }
